424a55446ab7fe15390492a2b3fa102b06892d96
PedroHolo <holodniakpedro@gmail.com>
2022-01-16 00:14:47 -0300

    component integracao

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -15,6 +15,11 @@ export default function Inicio() {
       <Navegador destino="/navegacao" texto="Navegação #01" cor="green" />
       <Navegador destino="/cliente/sp-2/123" texto="Navegação #02" cor="blue" />
       <Navegador destino="/estado" texto="componente com Estado" cor="pink" />
+      <Navegador
+        destino="/integracao"
+        texto="Integração com API #01"
+        cor="yellow"
+      />
     </div>
   );
 }
